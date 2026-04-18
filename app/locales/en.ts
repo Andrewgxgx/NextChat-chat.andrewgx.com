7876a9a4597aef9f208b5f922223fd2a81d0bc47
Andrew <andrewgx@tuta.io>
2026-04-18 19:57:40 +0630

    change system prompt

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -9,13 +9,10 @@ const en: LocaleType = {
   WIP: "Coming Soon...",
   Error: {
     Unauthorized: isApp
-      ? `😆 Oops, there's an issue. No worries:
-     \\ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
-     \\ 2️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️`
-      : `😆 Oops, there's an issue. Let's fix it:
-     \ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
-     \ 2️⃣ Using a private setup? [Click here](/#/auth) to enter your key 🔑
-     \ 3️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️
+      ? '
+      \ Hey! Welcome to [Andrew's NextChat](chat.andrewgx.com)
+     \  [Click here](/#/auth) to enter your access key 🔑
+     \  ⚙️
      `,
   },
   Auth: {
@@ -28,7 +25,7 @@ const en: LocaleType = {
     Later: "Later",
     SaasTips: "Too Complex, Use Immediately Now",
     TopTips:
-      "🥳 NextChat AI launch promotion: Instantly unlock the latest models like OpenAI o1, GPT-4o, Claude-3.5!",
+      "🥳 GGs",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} messages`,
