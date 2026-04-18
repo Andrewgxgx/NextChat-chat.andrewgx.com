a181389572ab7a95a4daea0d9e874d37476f7ffc
Andrew <andrewgx@tuta.io>
2026-04-18 20:03:16 +0630

    Update metadata for application title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "GX CHAT ",
+  description: "Your personal gateway to 20+ AI MODELS.",
   appleWebApp: {
-    title: "NextChat",
+    title: "GX CHAT",
     statusBarStyle: "default",
   },
 };
